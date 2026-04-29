244c42a1ed86800ca5ead4384bd567a4b88157a0
Vítor <vitorhugo8899o@gmail.com>
2026-04-29 15:57:15 -0300

    fix: retirando a rota de projetos

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
-import Projects from './Pages/Project.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  },
-  {
-    path: '/Projects',
-    element: <Projects />
-  }
-])
-
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
